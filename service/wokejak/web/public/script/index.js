05bfddf6ecce6f06669e5b355312c70ce0238b66
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-08-27 05:38:29 -0700

    wip

--- service/wokejak/web/public/script/index.js
+++ service/wokejak/web/public/script/index.js
@@ -1,51 +1,62 @@
-const boxBtn2 = document.querySelector("#box-btn2");
-boxBtn2.addEventListener("click", function () {
-    const b = document.querySelector("#test-box");
-    b.classList.replace("first", "second")
-});
+/** @type {Record<string, HTMLButtonElement>} */
+const contentBtns = {
+    "home-btn": document.querySelector("#home-btn"),
+    "about-btn": document.querySelector("#about-btn"),
+    "contact-btn": document.querySelector("#contact-btn"),
+};
 
-{
-    /** @type {Record<string, HTMLElement>} */
-    const contentBlocks = {
-        "home-content": document.querySelector("#home-content"),
-        "about-content": document.querySelector("#about-content"),
-        "contact-content": document.querySelector("#contact-content"),
-    };
+/** @type {Record<string, HTMLButtonElement>} */
+const selectedContentBtns = {
+    "home-btn-selected": document.querySelector("#home-btn-selected"),
+    "about-btn-selected": document.querySelector("#about-btn-selected"),
+    "contact-btn-selected": document.querySelector("#contact-btn-selected"),
+};
 
-    /** @type {Record<string, HTMLButtonElement>} */
-    const contentBtns = {
-        "home-btn": document.querySelector("#home-btn"),
-        "about-btn": document.querySelector("#about-btn"),
-        "contact-btn": document.querySelector("#contact-btn"),
-    };
+/** @type {HTMLElement} */
+const header = document.querySelector("#header");
+/** @type {HTMLElement} */
+const footer = document.querySelector("#footer");
 
-    /** @type {Record<string, HTMLButtonElement>} */
-    const selectedContentBtns = {
-        "home-btn-selected": document.querySelector("#home-btn-selected"),
-        "about-btn-selected": document.querySelector("#about-btn-selected"),
-        "contact-btn-selected": document.querySelector("#contact-btn-selected"),
-    };
+/** @type {HTMLElement} */
+const carouselWrapper = document.querySelector("#carousel-wrapper");
+/** @type {HTMLElement} */
+const carousel = document.querySelector("#carousel");
 
-    for (const contentButton in contentBtns) {
-        contentBtns[contentButton].addEventListener("click", function (event) {
-            const target = /** @type {HTMLButtonElement} */ (event.target);
-            const targetBlock = target.id.split("-")[0] + "-content";
+carouselWrapper.style.height = `calc(100vh - ${header.clientHeight + footer.clientHeight}px)`;
+carousel.style.height = `calc(100vh - ${header.clientHeight + footer.clientHeight}px)`;
+let carouselState = "first";
 
-            contentBlocks[targetBlock].style.display = "block";
-            for (const contentBlock in contentBlocks) {
-                if (contentBlock === targetBlock) continue;
-                contentBlocks[contentBlock].style.display = "none";
+for (const contentBtn in contentBtns) {
+    contentBtns[contentBtn].addEventListener("click", function () {
+        const btn = contentBtns[contentBtn];
+
+        for (const contentBtn in contentBtns) {
+            if (contentBtn === btn.id) {
+                selectedContentBtns[contentBtn + "-selected"].style.display = "block";
+                contentBtns[contentBtn].style.display = "none";
+            } else {
+                selectedContentBtns[contentBtn + "-selected"].style.display = "none";
+                contentBtns[contentBtn].style.display = "block";
             }
+        }
 
-            for (const contentBtn in contentBtns) {
-                if (contentBtn === target.id) {
-                    selectedContentBtns[contentBtn + "-selected"].style.display = "block";
-                    contentBtns[contentBtn].style.display = "none";
-                } else {
-                    selectedContentBtns[contentBtn + "-selected"].style.display = "none";
-                    contentBtns[contentBtn].style.display = "block";
-                }
-            }
-        });
-    }
+        switch (btn.id) {
+            case "home-btn":
+                carousel.classList.replace(carouselState, "first");
+                carouselState = "first";
+                break;
+            case "about-btn":
+                carousel.classList.replace(carouselState, "second");
+                carouselState = "second";
+                break;
+            case "contact-btn":
+                carousel.classList.replace(carouselState, "third");
+                carouselState = "third";
+                break;
+
+            default:
+                break;
+        }
+    });
 }
+
