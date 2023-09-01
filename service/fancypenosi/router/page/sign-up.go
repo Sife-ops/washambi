263dc3e4b83c20c6ccfae7e4023add060374f40e
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-09-01 03:13:16 -0700

    wip

--- service/fancypenosi/router/page/sign-up.go
+++ service/fancypenosi/router/page/sign-up.go
@@ -21,7 +21,11 @@ func Account(aa AccountAction) http.HandlerFunc {
 		bgs := []string{"forest", "city"}
 
 		web.
-			Parse("page/account.html", "partial/sign-in.html", "partial/sign-up.html").
+			Parse(
+				"page/account.html",
+				"partial/sign-in.html",
+				"partial/sign-up.html",
+			).
 			Execute(w, map[string]interface{}{
 				"accountAction": aa,
 				"styles":        []string{"sign-up"},
