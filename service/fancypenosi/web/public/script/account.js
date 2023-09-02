6c97c808f5bc496c8574860f9a5023e43e6b99a5
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-09-01 17:36:20 -0700

    wip

--- service/fancypenosi/web/public/script/account.js
+++ service/fancypenosi/web/public/script/account.js
@@ -1,6 +1,8 @@
+/** @type {HTMLElement} */
+const carousel = document
+    .querySelector("#carousel");
 // setTimeout(() => {
-document
-    .querySelector("#carousel")
+carousel
     .classList
     .remove("retracted");
 // }, 1000);
@@ -26,9 +28,21 @@ document
 ////////////////////////////////////////////////////////////////////////////////
 
 /** @type {HTMLInputElement} */
+const signUpEmail = document.querySelector("#sign-up-email");
+/** @type {HTMLInputElement} */
 const signUpPassword = document.querySelector("#sign-up-password");
 /** @type {HTMLInputElement} */
 const confirmPassword = document.querySelector("#confirm-password");
+/** @type {HTMLButtonElement} */
+const signUpSubmit = document.querySelector("#sign-up-submit");
+/** @type {HTMLButtonElement} */
+const focusSignIn = document.querySelector("button.focus-sign-in");
+/** @type {HTMLElement} */
+const signUpSuccess = document.querySelector("#sign-up-success");
+/** @type {HTMLElement} */
+const signUpError = document.querySelector("#sign-up-error");
+/** @type {HTMLElement} */
+const signUpErrorText = document.querySelector("#sign-up-error-text");
 
 const passwordRegex = new RegExp(
     "^(?=.*[0-9])(?=.*[_!@#$%^&*])[a-zA-Z0-9_!@#$%^&*]{8,32}$"
@@ -61,26 +75,13 @@ document
     .addEventListener("submit", async function (event) {
         event.preventDefault();
 
-        /** @type {HTMLInputElement} */
-        const signUpEmail = document.querySelector("#sign-up-email");
-        /** @type {HTMLButtonElement} */
-        const signUpSubmit = document.querySelector("#sign-up-submit");
-        /** @type {HTMLButtonElement} */
-        const focusSignIn = document.querySelector("button.focus-sign-in");
-        /** @type {HTMLElement} */
-        const signUpSuccess = document.querySelector("#sign-up-success");
-        /** @type {HTMLElement} */
-        const signUpError = document.querySelector("#sign-up-error");
-        /** @type {HTMLElement} */
-        const signUpErrorText = document.querySelector("#sign-up-error-text");
-
         signUpEmail.readOnly = true;
         signUpPassword.readOnly = true;
         confirmPassword.readOnly = true;
         signUpSubmit.disabled = true;
         focusSignIn.disabled = true;
-        signUpError.style.display = "none";
         signUpSuccess.style.display = "none";
+        signUpError.style.display = "none";
         // todo: spinner
 
         // const res = await fetch("/sign-up", {
@@ -112,7 +113,6 @@ document
                 return;
             }
 
-            signUpError.style.display = "block";
             switch (res.statusText) {
                 case "Conflict":
                     signUpErrorText.innerText = "An account with that e-mail already exists!";
@@ -121,8 +121,69 @@ document
                     signUpErrorText.innerText = "An unkown error occurred. Please try again later.";
                     break;
             }
+            signUpError.style.display = "block";
         }, 1000)
     });
 
 ////////////////////////////////////////////////////////////////////////////////
-// todo: sign-in
+
+/** @type {HTMLInputElement} */
+const signInEmail = document.querySelector("#sign-in-email");
+/** @type {HTMLInputElement} */
+const signInPassword = document.querySelector("#sign-in-password");
+/** @type {HTMLButtonElement} */
+const signInSubmit = document.querySelector("#sign-in-submit");
+/** @type {HTMLButtonElement} */
+const focusSignUp = document.querySelector("button.focus-sign-up");
+/** @type {HTMLElement} */
+const signInSuccess = document.querySelector("#sign-in-success");
+/** @type {HTMLElement} */
+const signInError = document.querySelector("#sign-in-error");
+/** @type {HTMLElement} */
+const signInErrorText = document.querySelector("#sign-in-error-text");
+
+document
+    .querySelector("#sign-in-form")
+    .addEventListener("submit", async function (event) {
+        event.preventDefault();
+
+        signInEmail.readOnly = true;
+        signInPassword.readOnly = true;
+        signInSubmit.disabled = true;
+        focusSignUp.disabled = true;
+        signInSuccess.style.display = "none";
+        signInError.style.display = "none";
+
+        // todo: fetch
+        const res = {
+            ok: true
+        }
+
+        setTimeout(() => {
+            signInEmail.readOnly = false;
+            signInPassword.readOnly = false;
+            signInSubmit.disabled = false;
+            focusSignUp.disabled = false;
+
+            if (res.ok) {
+                signInSuccess.style.display = "block";
+                setTimeout(() => {
+                    // console.log("okokok")
+                    const tx = carousel
+                        .classList
+                        .contains("first") ? "0vw" : "-100vw";
+                    carousel.style.transform = `translate(${tx}, -100vh)`;
+                }, 1000);
+                return
+            }
+
+            switch (res.statusText) {
+                case "todo":
+                    break;
+
+                default:
+                    break;
+            }
+            signInError.style.display = "block";
+        }, 1000);
+    })
